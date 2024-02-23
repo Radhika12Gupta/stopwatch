8c21dcdb13d0964e4c6394fe8c0ad29d6a7e617e
Radhika Gupta <radhikagupta@Radhikas-MacBook-Pro.local>
2024-02-23 20:22:21 +0530

    error resolved

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -3,12 +3,11 @@ import React, { useState ,useEffect} from 'react'
 const Stopwatch = () => {
     const [time,setTime]=useState(0)
     const [isActive, setIsActive] = useState(false);
-    const [isPaused, setIsPaused] = useState(true);
 
     useEffect(() => {
         let interval = null;
  
-        if (isActive && isPaused === false) {
+        if (isActive) {
             interval = setInterval(() => {
                 setTime((time) => time + 10);
             }, 10);
@@ -18,24 +17,29 @@ const Stopwatch = () => {
         return () => {
             clearInterval(interval);
         };
-    }, [isActive, isPaused])
+    }, [isActive])
 
   return (
     <div>
 
         <h1>Stopwatch</h1>
-        Time:<span className="digits">
+        Time: <span className="digits">
                 {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
             </span>
              <span className="digits">
                 {("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
             </span>
-<br/>
-       {isActive ? (<button onClick={(e)=>{setIsPaused(!isPaused)
-       setIsActive(false)}
-       }>Stop</button>):(<button onClick={(e)=>{setIsActive(true)
-    setIsPaused(false)}
-    }>Start</button>)} 
+            {/* <span className="digits mili-sec">
+                {("0" + ((time / 10) % 100)).slice(-2)}
+            </span> */}
+        <br/>
+       {isActive ? 
+       (<button onClick={(e)=>{setIsActive(false)}
+       }>Stop</button>)
+       :
+       (<button onClick={(e)=>{setIsActive(true)}
+    }>Start</button>)
+    } 
         <button onClick={(e)=>{ 
             setTime(0)
          setIsActive(false)}
