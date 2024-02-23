89eaf72070a7dd8eec5f55a191f4ec7fddc1383f
Radhika Gupta <radhikagupta@Radhikas-MacBook-Pro.local>
2024-02-23 16:57:14 +0530

    testcase handled

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -24,13 +24,13 @@ const Stopwatch = () => {
     <div>
 
         <h1>Stopwatch</h1>
-        Time: <span className="digits">
+        Time:<span className="digits">
+                {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+            </span>
+             <span className="digits">
                 {("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
             </span>
-            <span className="digits mili-sec">
-                {("0" + ((time / 10) % 100)).slice(-2)}
-            </span>
-       <br/>
+<br/>
        {isActive ? (<button onClick={(e)=>{setIsPaused(!isPaused)
        setIsActive(false)}
        }>Stop</button>):(<button onClick={(e)=>{setIsActive(true)
