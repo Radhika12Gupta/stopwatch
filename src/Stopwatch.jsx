1c05709930fdaad2de7f3ae742d869c9fde4aa66
Radhika Gupta <radhikagupta@Radhikas-MacBook-Pro.local>
2024-02-23 20:47:29 +0530

    final commit

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -23,8 +23,8 @@ const Stopwatch = () => {
     <div>
 
         <h1>Stopwatch</h1>
-        Time: <span className="digits">
-                {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+        Time:<span className="digits">
+                {(" "+Math.floor((time / 60000) % 60)).slice(-2)}:
             </span>
              <span className="digits">
                 {("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
